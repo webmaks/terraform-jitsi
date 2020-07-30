63e38431d866a6d87b182d76215c46eea51c4104
Maksimka <maksimka@iMac-Maksimka.local>
2020-07-30 21:25:21 +0600

    first commit

---
+++ setup_s3_and_dynDB.tf
@@ -0,0 +1,40 @@
+i# terraform state file setup
+# create an S3 bucket to store the state file in
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+  bucket = "jitsi-terraform-state-s3"
+  region = "us-east-2"
+
+  versioning {
+    # enable with caution, makes deleting S3 buckets tricky
+    enabled = false
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    name = "S3 Remote Terraform State Store"
+    proj = "jitsi"
+    env = "prod"
+  }
+}
+
+# create a DynamoDB table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "jitsi-terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    name = "DynamoDB Terraform State Lock Table"
+    proj = "jitsi"
+    env = "prod"
+  }
+}
